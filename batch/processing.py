33582baa47235379c005e50be4a90d66464d016f
Ognjen <ognjenkuzmanovic1@gmail.com>
2022-01-10 23:17:57 +0100

    [feat]: Deleted unneccessary lines

--- batch/processing.py
+++ batch/processing.py
@@ -25,140 +25,121 @@ df = spark.read \
   .csv(HDFS_NAMENODE + "/user/root/data-lake/transformation/batch-preprocessed.csv")
 
 
-# print("--- 1. Average time to sell a car ---")
-# priceDiff = df.dropDuplicates(["vin", "firstSeen"]) \
-#     .select(
-#         col("firstSeen"), 
-#         col("lastSeen"), 
-#         datediff(col("lastSeen"), col("firstSeen")).alias("date_difference")
-#     ) \
-#     .agg(
-#         round(avg("date_difference")).alias("AvgSellTime(days)"))
+print("--- 1. Average time to sell a car ---")
+df.dropDuplicates(["vin", "firstSeen"]) \
+    .select(
+        col("firstSeen"), 
+        col("lastSeen"), 
+        datediff(col("lastSeen"), col("firstSeen")).alias("date_difference")
+    ) \
+    .agg(
+        round(avg("date_difference")).alias("AvgSellTime(days)")
+    ) \
+    .show()
 
-# priceDiff.show()
+
+print("--- 2. Average price difference between asking price and msrp ---")
+df.filter("msrp > 0 and askPrice > 0") \
+              .selectExpr("(1 - (askPrice/msrp)) * 100 as price_difference") \
+              .agg(
+                  round(avg("price_difference")).alias("AvgPriceDiff(%)")
+              ) \
+              .show()
 
 
-# print("--- 2. Average price difference between asking price and msrp ---")
-# priceDiff = df.filter("msrp > 0 and askPrice > 0") \
-#               .selectExpr("(1 - (askPrice/msrp)) * 100 as price_difference") \
-#               .agg(
-#                   round(avg("price_difference")).alias("AvgPriceDiff(%)"))
+print("--- 3. Mileage validity ---")
+mileage_df = df.dropDuplicates(["vin", "firstSeen"]) \
+       .filter("msrp > 0") \
+       .filter("not (mileage == 0 and isNew == false)")
 
-# priceDiff.show()
+mileage_df.select("*") \
+  .groupBy("vin") \
+  .agg(count("*").alias("count")) \
+  .orderBy(desc("count")) \
+  .show(truncate=False)
 
-# print("--- 3. Mileage validity ---")
-# df = df.dropDuplicates(["vin", "firstSeen"]) \
-#        .filter("msrp > 0") \
-#        .filter("not (mileage == 0 and isNew == false)")
+mileage_df.filter("vin == '06d4b59fdef1d3e3ac3c8b10596f73ea4720355857fbd000f9f6921bc61e8e87'").show()
 
-# df.select("*") \
-#   .groupBy("vin") \
-#   .agg(count("*").alias("count")) \
-#   .orderBy(desc("count")) \
-#   .show(truncate=False)
 
-# df.filter("vin == '06d4b59fdef1d3e3ac3c8b10596f73ea4720355857fbd000f9f6921bc61e8e87'") \
-#   .orderBy(desc("firstSeen")).show()
+print("--- 4. Price decline per year for specific model ---")
+window = Window.partitionBy().orderBy("modelName", "vf_ModelYear")
 
-# mileageCheck = df.groupBy("vin").count().show(100)
+price_decline_df = df.filter((col("askPrice") > 0) & (col("askPrice") < 500000)) \
+    .filter("modelName == 'Corolla'") \
+    .groupBy("modelName", "vf_ModelYear")\
+    .agg(
+        min("askPrice").alias("MinPrice"),
+        max("askPrice").alias("MaxPrice"),
+        round(mean("askPrice")).alias("AvgPrice"),
+        count("*").alias("TotalCars")
+    ) \
+    .withColumn("PriceDiff", (col("AvgPrice") - lag("AvgPrice", -1).over(window))) \
+    .orderBy(desc("vf_ModelYear"))
 
-# df.where("vin == '01843d4a7c85f8cb473cef6b5fd6502a7f78b527cc8e119ad8c47cbf2f40adba'").show()
+df_index = price_decline_df.select("*").withColumn("id", monotonically_increasing_id())
+df_index.drop("id").show()
 
-# df.filter("vin == '01843d4a7c85f8cb473cef6b5fd6502a7f78b527cc8e119ad8c47cbf2f40adba'").show()
+df_index.filter(col("id") < 5) \
+  .agg(round(mean("PriceDiff")).alias("AvgPriceDeclinePerYearInFirstFiveYears($)")) \
+  .show()
 
-# df.groupBy("vin").count().show(300, truncate=False)
+df_index.filter(col("id") >= 5) \
+  .agg(round(mean("PriceDiff")).alias("AvgPriceDeclinePerYearAfterFirstFiveYears($)")) \
+  .show()
 
-# df.select(countDistinct("vin")).show()
-# print(df.count())
 
-# print("--- 4. Price decline per year for specific model ---")
-# window = Window.partitionBy().orderBy("modelName", "vf_ModelYear")
+print("--- 6. Average mileage per year ---")
+df.dropDuplicates(["vin"]) \
+       .filter("not (mileage == 0 and isNew == false)") \
+       .filter("vf_ModelYear < 2021") \
+       .groupBy("vf_ModelYear") \
+       .agg(
+         round(mean("mileage")).alias("AvgMileage")
+       ) \
+       .orderBy(desc("vf_ModelYear")) \
+       .show()
 
-# df = df.filter((col("askPrice") > 0) & (col("askPrice") < 500000)) \
-#     .filter("modelName == 'Corolla'") \
-#     .groupBy("modelName", "vf_ModelYear")\
-#     .agg(
-#         min("askPrice").alias("MinPrice"),
-#         max("askPrice").alias("MaxPrice"),
-#         round(mean("askPrice")).alias("AvgPrice"),
-#         count("*").alias("TotalCars")
-#     ) \
-#     .withColumn("PriceDiff", (col("AvgPrice") - lag("AvgPrice", -1).over(window))) \
-#     .orderBy(desc("vf_ModelYear"))
 
-# df_index = df.select("*").withColumn("id", monotonically_increasing_id())
-# df_index.show()
+print("--- 7. Months sorted by number of ads ---")
+df.withColumn("PostingYear", year(col("firstSeen"))) \
+  .withColumn("PostingMonth", month(col("firstSeen"))) \
+  .groupBy("PostingYear", "PostingMonth") \
+  .agg(
+    count("*").alias("NumberOfAds"),
+    first("firstSeen").alias("date")
+  ) \
+  .filter(
+    ( col("date") >= lit('2018-06-01').cast(DateType()) )  
+    & ( col("date") <= lit('2020-06-30').cast(DateType()) )
+    ) \
+  .orderBy(desc("NumberOfAds")) \
+  .drop("date") \
+  .show()
 
-# df_index.filter(col("id") < 5) \
-#   .agg(round(mean("PriceDiff")).alias("AvgPriceDeclinePerYearInFirstFiveYears")) \
-#   .show()
 
-# df_index.filter(col("id") >= 5) \
-#   .agg(round(mean("PriceDiff")).alias("AvgPriceDeclinePerYearAfterFirstFiveYears")) \
-#   .show()
+print("--- 8. Most advertised car brands per year ---")
+df_brands = df.dropDuplicates(["vin"]) \
+  .withColumn("PostingYear", year(col("firstSeen"))) \
+  .groupBy("PostingYear", "brandName") \
+  .agg(
+    count("*").alias("Total"), 
+  )
 
-# df.filter((col("modelName") == 'Q7') & (col("vf_ModelYear") == 2007)).orderBy(desc("askPrice")).show(85, truncate=True)
+df_brands_sorted = df_brands.groupBy("PostingYear") \
+  .agg(
+    max("Total").alias("NumberOfAds")
+  )  
 
-# print("--- 6. Average mileage per year ---")
-# df.dropDuplicates(["vin"]) \
-#        .filter("not (mileage == 0 and isNew == false)") \
-#        .filter("vf_ModelYear < 2021") \
-#        .groupBy("vf_ModelYear") \
-#        .agg(
-#          round(mean("mileage")).alias("AvgMileage")
-#        ) \
-#        .orderBy(desc("vf_ModelYear")) \
-#        .show()
+df_brands \
+  .join(df_brands_sorted) \
+  .where(col("NumberOfAds") == col("Total")) \
+  .drop(df_brands.PostingYear) \
+  .drop(col("Total")) \
+  .orderBy(desc("PostingYear")) \
+  .show()
 
-# print("--- 7. Months sorted by number of ads ---")
-# df.withColumn("PostingYear", year(col("firstSeen"))) \
-#   .withColumn("PostingMonth", month(col("firstSeen"))) \
-#   .groupBy("PostingYear", "PostingMonth") \
-#   .agg(
-#     count("*").alias("NumberOfAds"),
-#     first("firstSeen").alias("date")
-#   ) \
-#   .filter(
-#     ( col("date") >= lit('2018-06-01').cast(DateType()) )  
-#     & ( col("date") <= lit('2020-06-30').cast(DateType()) )
-#     ) \
-#   .orderBy(desc("NumberOfAds")) \
-#   .drop("date") \
-#   .show(30)
-
-# print("--- 8. Most advertised car brands per year ---")
-# df_brands = df.dropDuplicates(["vin"]) \
-#   .withColumn("PostingYear", year(col("firstSeen"))) \
-#   .groupBy("PostingYear", "brandName") \
-#   .agg(
-#     count("*").alias("Total"), 
-#   )
-
-# df_brands_sorted = df_brands.groupBy("PostingYear") \
-#   .agg(
-#     max("Total").alias("NumberOfAds")
-#   )  
-
-# df_brands \
-#   .join(df_brands_sorted) \
-#   .where(col("NumberOfAds") == col("Total")) \
-#   .drop(df_brands.PostingYear) \
-#   .drop(col("Total")) \
-#   .orderBy(desc("PostingYear")) \
-#   .show(100)
   
-# JUNK CODE
-# window = Window.partitionBy("PostingYear", "brandName")
-
-# df.dropDuplicates(["vin"]) \
-#   .filter("not (mileage == 0 and isNew == false)") \
-#   .withColumn("PostingYear", year(col("firstSeen"))) \
-#   .withColumn("PostingMonth", month(col("firstSeen"))) \
-#   .withColumn("Total", count("*").over(window)) \
-#   .withColumn("Max", max("Total").over(window)) \
-#   .where(col("Total") == col("Max")) \
-#   .show()
-
+print("--- 9. Average number of seats joined with number of children born ---")
 df_children = spark.read \
   .option("delimiter", ",") \
   .option("header", "true") \
@@ -178,8 +159,3 @@ df.dropDuplicates(["vin"]) \
   .filter("NumOfChildren is not null") \
   .drop("Year") \
   .show()
-
-
-
-
-
